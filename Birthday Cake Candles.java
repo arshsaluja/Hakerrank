52a83f2ddd3cec4590c7e46b26b68721d27d2937
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-07-03 00:05:54 +0530

    Update Birthday Cake Candles.java

--- Birthday Cake Candles.java
+++ Birthday Cake Candles.java
@@ -1,44 +1,22 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
-
-public class Solution {
-
-    // Complete the birthdayCakeCandles function below.
-    static int birthdayCakeCandles(int[] ar) {
-
-
-    }
-
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int arCount = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        int[] ar = new int[arCount];
-
-        String[] arItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        for (int i = 0; i < arCount; i++) {
-            int arItem = Integer.parseInt(arItems[i]);
-            ar[i] = arItem;
+class Solution
+{
+    public static void main(String args[])
+    {Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i =0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
         }
-
-        int result = birthdayCakeCandles(ar);
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
-
-        scanner.close();
+        int max = arr[0], count = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        } else if (arr[i] == max) {
+            count++;
+        }
+    }
+    System.out.println(count);
     }
 }

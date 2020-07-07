02192b920faae830966f69bd8cb7f536d9ec55b6
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-07-07 16:00:37 +0530

    Update Super Reduced String.java

--- Super Reduced String.java
+++ Super Reduced String.java
@@ -12,29 +12,23 @@ public class Solution {
     public static void main(String args[]) {
         Scanner sc= new Scanner(System.in);
         String s= sc.next();
-        String t="";
         
-for(int i=0;i<s.length();i++)
+        
+for(int i=1;i<s.length();i++)
 {
-    for(int j=i+1;j<s.length();j++)
-    {
-    if(s.charAt(i)==s.charAt(j))
-    {
-        i++;
-        
-    }
-    else
-   t=t+s.charAt(i);
     
+    if(s.charAt(i)==s.charAt(i-1))
+    {
+    s=s.substring(0,i-1)+s.substring(i+1);
+    i=0;
     }
-}
-if(t.compareTo("")!=0)
+    }
+
+if(s.length()!=0)
         {
-    System.out.println(t);
+    System.out.println(s);
 }
 else
 System.out.println("Empty String");
-
-
     }
     }

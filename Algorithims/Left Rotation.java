c89af51cfa81132af777bc50df96da2877141d84
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-08-27 00:25:18 +0530

    Update Left Rotation.java

--- Algorithims/Left Rotation.java
+++ Algorithims/Left Rotation.java
@@ -1,34 +1,30 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int d=sc.nextInt();
+     int a[]=new int[n];
+     for(int i=0;i<n;i++)
+     {
+         a[i]=sc.nextInt();
+     }
 
-public class Solution {
-
-
-
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        String[] nd = scanner.nextLine().split(" ");
-
-        int n = Integer.parseInt(nd[0]);
-
-        int d = Integer.parseInt(nd[1]);
-
-        int[] a = new int[n];
-
-        String[] aItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        for (int i = 0; i < n; i++) {
-            int aItem = Integer.parseInt(aItems[i]);
-            a[i] = aItem;
-        }
-
-        scanner.close();
+   while(d!=0)
+         {
+              int temp=a[0];
+             for(int i=0;i<n-1;i++)
+             {
+         a[i]=a[i+1];
+             }
+         a[n-1]=temp;
+         d--;
+         }
+     for(int i=0;i<n;i++)
+     {
+         System.out.print(a[i]+" ");
+     }
     }
 }

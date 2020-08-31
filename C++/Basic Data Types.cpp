308104c2d54ce54f1207f76f548ddd5fb96123ec
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-08-31 22:03:13 +0530

    Create Basic Data Types.cpp

---
+++ C++/Basic Data Types.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int i;long b;char ch;float f;double d;
+
+    cin>>i>>b>>ch>>f>>d;
+    cout<<i<<endl<<b<<endl<<ch<<endl<<setprecision(3)<<fixed<<f <<endl<<setprecision(6)<<fixed<<d<<endl;
+    return 0;
+}
